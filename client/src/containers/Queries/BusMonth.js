d9a569851b7025f75878188cae91c3d24c97409a
Bryan Konshak <bkonshak@gmail.com>
2019-03-11 20:38:26 -0400

    BusReceived CSV working with save file

--- client/src/containers/Queries/BusMonth.js
+++ client/src/containers/Queries/BusMonth.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { reduxForm, Field } from "redux-form";
+import fileDownload from "react-file-download";
 import {
   Button,
   Form,

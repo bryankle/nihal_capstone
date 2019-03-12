a76a582e87f39b1fb706c0b581eb17b5342ae1d3
Bryan Konshak <bkonshak@gmail.com>
2019-03-12 19:19:36 -0400

    first google graph working for business totals

--- client/src/containers/Queries/BusTotal.js
+++ client/src/containers/Queries/BusTotal.js
@@ -17,6 +17,106 @@ import * as actions from "../../actions";
 import { Link } from "react-router-dom";
 import semanticFormField from "../../components/SemanticForm";
 import requireAdmin from "../requireAdmin";
+import Chart from "react-google-charts";
+import { convertCSVToArray } from "convert-csv-to-array";
+
+import ReactDOM from "react-dom";
+
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray(strData, strDelimiter) {
+  // Check to see if the delimiter is defined. If not,
+  // then default to comma.
+  strDelimiter = strDelimiter || ",";
+
+  // Create a regular expression to parse the CSV values.
+  var objPattern = new RegExp(
+    // Delimiters.
+    "(\\" +
+      strDelimiter +
+      "|\\r?\\n|\\r|^)" +
+      // Quoted fields.
+      '(?:"([^"]*(?:""[^"]*)*)"|' +
+      // Standard fields.
+      '([^"\\' +
+      strDelimiter +
+      "\\r\\n]*))",
+    "gi"
+  );
+
+  // Create an array to hold our data. Give the array
+  // a default empty first row.
+  var arrData = [[]];
+
+  // Create an array to hold our individual pattern
+  // matching groups.
+  var arrMatches = null;
+
+  // Keep looping over the regular expression matches
+  // until we can no longer find a match.
+  while ((arrMatches = objPattern.exec(strData))) {
+    // Get the delimiter that was found.
+    var strMatchedDelimiter = arrMatches[1];
+
+    // Check to see if the given delimiter has a length
+    // (is not the start of string) and if it matches
+    // field delimiter. If id does not, then we know
+    // that this delimiter is a row delimiter.
+    if (strMatchedDelimiter.length && strMatchedDelimiter != strDelimiter) {
+      // Since we have reached a new row of data,
+      // add an empty row to our data array.
+      arrData.push([]);
+    }
+
+    // Now that we have our delimiter out of the way,
+    // let's check to see which kind of value we
+    // captured (quoted or unquoted).
+    if (arrMatches[2]) {
+      // We found a quoted value. When we capture
+      // this value, unescape any double quotes.
+      var strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '"');
+    } else {
+      // We found a non-quoted value.
+      var strMatchedValue = arrMatches[3];
+    }
+
+    // Now that we have our value string, let's add
+    // it to the data array.
+    arrData[arrData.length - 1].push(strMatchedValue);
+  }
+
+  // Return the parsed data.
+  return arrData;
+}
+function doIt(myThingee) {
+  var keys = Object.keys(myThingee);
+  console.log("keys are here", keys);
+  var value;
+
+  for (var key in keys) {
+    value = parseInt(myThingee[key], 10);
+    console.log("Whats happening here in the for loop", value);
+
+    if (!isNaN(value)) {
+      myThingee[key] = value;
+      console.log("Whats happening here should be converting to number", keys);
+    }
+  }
+  return myThingee;
+}
+function formatChartData(data) {
+  //var
+  for (var i = 0, length = data.length; i < length; i++) {
+    console.log("data values", data[i][1]);
+    if (!isNaN(data[i][1])) {
+      data[i][1] = parseInt(data[i][1], 10);
+      console.log("data values", data[i].value1);
+      //data[i].value2 = parseFloat(data[i].value2);
+    }
+  }
+  return data;
+}
 const checkboxOptions = [
   {
     value: "1",
@@ -37,14 +137,18 @@ const adminOptions = [
   }
 ];
 
+var array = [];
 class BusTotal extends Component {
   constructor() {
     super();
     this.state = {
       user_id: "",
-      employees: []
+      employees: [],
+      fetching: true,
+      results: []
     };
   }
+
   componentDidMount() {
     this.setState({
       user_id: this.props.user_id
@@ -76,19 +180,70 @@ class BusTotal extends Component {
   */
   onSubmit = formProps => {
     console.log("look at the formprops", formProps);
+
     this.props.getAwardTotal().then(response => {
+      array = CSVToArray(response.data);
+      var datas = [
+        ["Element", "Density"],
+        ["Copper", 8.94], // RGB value
+        ["Silver", 10.49], // English color name
+        ["Gold", 19.3],
+        ["Platinum", 21.45] // CSS-style declaration
+      ];
+      //formatChartData(data)
+      var array2 = formatChartData(array);
+      //var array2 = doIt(array);
+      this.setState({ results: array2 });
+      console.log("this state results", this.state.results);
+      console.log("new array original", array);
+      console.log("new array after the conversion", array2);
+      console.log("and here is the data thats preloaded", datas);
       console.log("response says", response.data);
+
+      this.setState({ fetching: false });
       // TODO: Determine page to redirect to after admin adds user
-      this.props.history.push("/");
+      //this.props.history.push("/");
       fileDownload(response.data, "AwardTotals.csv");
+    });
+  };
+  renderresults() {
+    return (
+      <div className="App">
+        <Chart
+          chartType="ColumnChart"
+          width="100%"
+          height="400px"
+          data={this.state.results}
+          //data={datas}
+        />
+      </div>
+    );
+  }
+  handleReset = () => {
+    // since our views are dependent on `haveData` and `fetching`, we can just set those to false
+    this.setState({
+      fetching: false
+      //haveData: false
     });
   };
   render() {
     const { handleSubmit } = this.props; // handleSubmit provided by redux form
-
-    return (
-      <div className="login-form">
-        <style>{`
+    console.log("in the render this.state", this.state.results);
+    if (this.state.fetching == false) {
+      return (
+        <div className="App">
+          <Chart
+            chartType="ColumnChart"
+            width="100%"
+            height="400px"
+            data={this.state.results}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="login-form">
+          <style>{`
         body > div,
         body > div > div,
         body > div > div > div.login-form {
@@ -98,20 +253,21 @@ class BusTotal extends Component {
           padding-top: 5em
         }
       `}</style>
-        <Grid
-          textAlign="center"
-          style={{ height: "100%" }}
-          verticalAlign="middle"
-        >
-          {" "}
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as="h2" color="teal" textAlign="center">
-              Total Awards Received
-            </Header>
 
-            <Form size="large" onSubmit={handleSubmit(this.onSubmit)}>
-              <Segment stacked>
-                {/*
+          <Grid
+            textAlign="center"
+            style={{ height: "100%" }}
+            verticalAlign="middle"
+          >
+            {" "}
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Header as="h2" color="teal" textAlign="center">
+                Total Awards Received
+              </Header>
+
+              <Form size="large" onSubmit={handleSubmit(this.onSubmit)}>
+                <Segment stacked>
+                  {/*
                 <Field
                   name="first_name"
                   component={semanticFormField}
@@ -159,18 +315,18 @@ class BusTotal extends Component {
                   placeholder="Password"
                 />
                  */}
-                <Field
-                  name="awardsReceived"
-                  component={semanticFormField}
-                  as={Form.Dropdown}
-                  //icon="lock"
-                  options={checkboxOptions}
-                  placeholder="My Dropdown"
-                  iconPosition="left"
-                  type="text"
-                  placeholder="Award Type"
-                />{" "}
-                {/*}
+                  <Field
+                    name="awardsReceived"
+                    component={semanticFormField}
+                    as={Form.Dropdown}
+                    //icon="lock"
+                    options={checkboxOptions}
+                    placeholder="My Dropdown"
+                    iconPosition="left"
+                    type="text"
+                    placeholder="Award Type"
+                  />{" "}
+                  {/*}
                 <Field
                   name="admin"
                   component={semanticFormField}
@@ -183,18 +339,19 @@ class BusTotal extends Component {
                   placeholder="Admin or User"
                 />
                 */}
-                {
-                  //<div>{this.props.errorMessage}</div>}
-                }
-                <Button color="teal" fluid size="large">
-                  Submit
-                </Button>
-              </Segment>
-            </Form>
-          </Grid.Column>
-        </Grid>
-      </div>
-    );
+                  {
+                    //<div>{this.props.errorMessage}</div>}
+                  }
+                  <Button color="teal" fluid size="large">
+                    Submit
+                  </Button>
+                </Segment>
+              </Form>
+            </Grid.Column>
+          </Grid>
+        </div>
+      );
+    }
   }
 }
 
@@ -210,3 +367,5 @@ export default compose(
   reduxForm({ form: "query" }),
   requireAdmin
 )(BusTotal);
+//const rootElement = document.getElementById("root");
+//ReactDOM.render(<BusTotal />, rootElement);

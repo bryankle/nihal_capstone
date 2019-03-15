d5c11f25ed14c0d3f3b0db80676ac4760dfb91f3
Bryan Konshak <bkonshak@gmail.com>
2019-03-14 22:59:31 -0400

    css homepage

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
+import "../App.css";
 
 class Navbar extends Component {
   state = {};
@@ -146,6 +147,7 @@ class Navbar extends Component {
         inverted={fixed}
         secondary={!fixed}
         size="large"
+        color="grey"
       >
         <Container>
           <Menu.Item as="a" active>

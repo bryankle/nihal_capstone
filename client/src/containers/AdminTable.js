5fc656eb888a82484c588f8a59fd92b80a18aed6
bryankle <bryan@bryanle.io>
2019-03-17 13:48:49 -0400

    Change build path

--- client/src/containers/AdminTable.js
+++ client/src/containers/AdminTable.js
@@ -66,29 +66,28 @@ class AdminTable extends Component {
   }
   render() {
     console.log('props', this.props.users);
-    // const rows = this.props.users.length > 0 ? this.props.users.map(user => {
-    //   console.log(user)
-    //   const { user_id, email, first_name, last_name, region_id } = user;
-    //   console.log(user_id, email, first_name, last_name, region_id);
-    //   return (
-    //     <Table.Row>
-    //       <Table.Cell>{user_id}</Table.Cell>
-    //       <Table.Cell>{email}</Table.Cell>
-    //       <Table.Cell>{first_name === null ? 'None' : first_name} </Table.Cell>
-    //       <Table.Cell>{last_name === null ? 'None' : last_name} </Table.Cell>
-    //       <Table.Cell>{region_id} </Table.Cell>
-    //       <Table.Cell>
-    //         <Button primary>Edit</Button>
-    //         <Button negative>Delete</Button>
-    //       </Table.Cell>
-    //     </Table.Row>
-    //   )
-    // }) : [];
-    // console.log('rows', rows);
+    const rows = this.props.users.length > 0 ? this.props.users.map(user => {
+      console.log(user)
+      const { user_id, email, first_name, last_name, region_id } = user;
+      console.log(user_id, email, first_name, last_name, region_id);
+      return (
+        <Table.Row>
+          <Table.Cell>{user_id}</Table.Cell>
+          <Table.Cell>{email}</Table.Cell>
+          <Table.Cell>{first_name === null ? 'None' : first_name} </Table.Cell>
+          <Table.Cell>{last_name === null ? 'None' : last_name} </Table.Cell>
+          <Table.Cell>{region_id} </Table.Cell>
+          <Table.Cell>
+            <Button primary>Edit</Button>
+            <Button negative>Delete</Button>
+          </Table.Cell>
+        </Table.Row>
+      )
+    }) : [];
+    console.log('rows', rows);
       return (
           <div style={{'padding-top': '5em'}}>Welcome to the admin table.
-     
-
+    
             <Container>
                 <Table celled>
               <Table.Header>
@@ -103,7 +102,7 @@ class AdminTable extends Component {
               </Table.Header>
           
               <Table.Body>
-                {/* {rows} */}
+                {rows}
               </Table.Body>
           
               <Table.Footer>

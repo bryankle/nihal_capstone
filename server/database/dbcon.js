b529a43649a8bc8538083f3866813aec47d90424
bryankle <bryan@bryanle.io>
2019-03-16 22:04:16 -0400

    Production DB

--- server/database/dbcon.js
+++ server/database/dbcon.js
@@ -1,9 +1,11 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 const pool = mysql.createConnection({
   connectionLimit: 10,
-  host: "127.0.0.1",
-  user: "root",
-  password: "password",
-  database: "nihal"
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'bac08bb4a926ea',
+  password: '8c6dd9b0',
+  database: 'heroku_d01e475a4fa2c97'
 });
 module.exports.pool = pool;
+
+
